36b2090a5a3dcf10c7f7edee8f69c8d5d35e778a
SABITHA0001 <109308288+SABITHA0001@users.noreply.github.com>
2022-12-19 09:42:46 +0530

    Update Welcomejava19.java

--- Welcomejava19.java
+++ Welcomejava19.java
@@ -1,5 +1,6 @@
 public class Welcomejava19 {
     public static void main(String[] args) {
         System.out.println("welcome first version");
+        System.out.println("added at git secondline");
     }
 }
